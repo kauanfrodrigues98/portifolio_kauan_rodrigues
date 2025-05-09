e5d5a1403c57bf8fdba15b5a23c2c263b6c1719f
Kauan Rodrigues <kauanfrodrigues98@gmail.com>
2025-05-09 20:24:37 -0300

    remover logo

--- components/header/logo.vue
+++ components/header/logo.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="wsmobileheader clearfix">
         <!-- <NuxtLink to="/"><span class="smllogo"><img src="/assets/images/logo-pink.png" alt="mobile-logo" /></span></NuxtLink> -->
-        <NuxtLink to="/" class="logo-black"><span class="smllogo"><img src="/assets/images/logo-pink.png" alt="mobile-logo" /></span></NuxtLink>
-        <NuxtLink to="/" class="logo-white"><span class="smllogo"><img src="/assets/images/logo-white.png" alt="logo" /></span></NuxtLink>
+<!--        <NuxtLink to="/" class="logo-black"><span class="smllogo"><img src="/assets/images/logo-pink.png" alt="mobile-logo" /></span></NuxtLink>-->
+<!--        <NuxtLink to="/" class="logo-white"><span class="smllogo"><img src="/assets/images/logo-white.png" alt="logo" /></span></NuxtLink>-->
         <a id="wsnavtoggle" class="wsanimated-arrow" @click="toggleMobileMenu">
             <span></span>
         </a>
